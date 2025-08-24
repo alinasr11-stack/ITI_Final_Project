011fec8c96e7973b1c7fffe174a3c484465fc7bb
alinasr11-stack <gccjkjfd@gmail.com>
2025-08-24 20:50:19 +0300

    Upload Project

---
+++ ItiProject_GRD.DLL/Repository/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using ItiProject_GRD.DLL.Data;
+using ItiProject_GRD.DLL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItiProject_GRD.DLL.Repository
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext context) : base(context) { }
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+    }
+
+}
