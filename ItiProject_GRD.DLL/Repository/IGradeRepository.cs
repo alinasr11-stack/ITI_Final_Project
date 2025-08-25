dcef9fb9f182acdd62d2400fd4f90a7392048640
alinasr11-stack <gccjkjfd@gmail.com>
2025-08-25 23:26:55 +0300

    create

---
+++ ItiProject_GRD.DLL/Repository/IGradeRepository.cs
@@ -0,0 +1,14 @@
+﻿using ItiProject_GRD.DLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItiProject_GRD.DLL.Repository
+{
+    public interface IGradeRepository : IGenericRepository<Grade>
+    {
+        Task<IEnumerable<Grade>> GetGradesByTraineeIdAsync(int traineeId);
+    }
+}
---
+++ ItiProject_GRD.DLL/Repository/SessionRepository.cs
@@ -0,0 +1,21 @@
+﻿using ItiProject_GRD.DLL.Data;
+using ItiProject_GRD.DLL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItiProject_GRD.DLL.Repository
+{
+    public class SessionRepository : GenericRepository<Session>, ISessionRepository
+    {
+        public SessionRepository(AppDbContext context) : base(context) { }
+
+        public async Task<IEnumerable<Session>> GetByCourseIdAsync(int courseId)
+        {
+            return await _context.Sessions.Where(s => s.CourseId == courseId).ToListAsync();
+        }
+    }
+}
