a97503bb4f3cb995dc33353dca9ba52ba18060ab
alinasr11-stack <gccjkjfd@gmail.com>
2025-08-25 23:51:37 +0300

    Add Business Layer correctly after removing internal .git

---
+++ ItiProject_GRD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ItiProject_GRD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
